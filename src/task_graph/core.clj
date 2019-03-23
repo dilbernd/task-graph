b242081c051e215a297d22c07f548aa60ee203d7
Bernd Haug <haug@berndhaug.net>
2019-03-23 23:35:01 +0100

    Use stdlib, associative destructuring
    What a fun language, and much more readable if you actually use it
    occasionally enough to remember things.

--- src/task_graph/core.clj
+++ src/task_graph/core.clj
@@ -4,11 +4,12 @@
 (def test-data {"b" #{"a"} "k" #{"a", "c"} "c" #{"m"} "h" #{"b"} "j" #{"b"} "i" #{"b" "k"} "d" #{"c"} "e" #{"c"}
                 "f" #{"e"} "g" #{"d" "e"} "l" #{"g" "e"} "n" #{}})
 
-(defn acc-set "Adds new-value to map acc for key." [acc key new-value] (assoc acc key (conj (acc key #{}) new-value)))
+(defn acc-set "Adds another-val to set for key in map acc." [acc key another-val]
+  (update acc key #(conj (or % #{}) another-val)))
 
 (defn process-entry "Adds a dependency graph node's information to map acc." [acc [key val]]
   (if (empty? val) (acc-set acc :singles key)
-                   (assoc (acc-set acc :dependers key) :dependencies (sets/union (:dependencies acc) val))))
+                   (-> acc (acc-set :dependers key) (update :dependencies #(sets/union % val)))))
 
 (defn node-by-types "Summarizes a dependency graph into sets by node type." [inputs] (reduce process-entry {} inputs))
 
@@ -22,6 +23,6 @@
     tasklist))
 
 (defn execution-list "Summarize graph; find start nodes; calculate executable task list itself." [dependers-map]
-  (let [categorised (node-by-types dependers-map)
-        starts      (sets/union (:singles categorised) (sets/difference (:dependencies categorised) (:dependers categorised)))]
-    (-execution-list (into [] starts) starts (:dependers categorised) dependers-map)))
+  (let [{singles :singles dependers :dependers dependencies :dependencies} (node-by-types dependers-map)
+        starts (sets/union singles (sets/difference dependencies dependers))]
+    (-execution-list (into [] starts) starts dependers dependers-map)))
