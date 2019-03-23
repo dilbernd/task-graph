2a008f8111670134de5a4fb7dfc850ef09037225
Bernd Haug <haug@berndhaug.net>
2019-03-23 23:03:41 +0100

    Polish turd

--- src/task_graph/core.clj
+++ src/task_graph/core.clj
@@ -4,16 +4,15 @@
 (def test-data {"b" #{"a"} "k" #{"a", "c"} "c" #{"m"} "h" #{"b"} "j" #{"b"} "i" #{"b" "k"} "d" #{"c"} "e" #{"c"}
                 "f" #{"e"} "g" #{"d" "e"} "l" #{"g" "e"} "n" #{}})
 
-(defn acc-set [acc key new-value] (assoc acc key (conj (acc key #{}) new-value)))
+(defn acc-set "Adds new-value to map acc for key." [acc key new-value] (assoc acc key (conj (acc key #{}) new-value)))
 
-(defn process-entry [acc [key val]]
-  (cond (empty? val) (acc-set acc :singles key)
-        :💩 (reduce #(acc-set %1 :dependencies %2) (acc-set acc :dependers key) val)))
+(defn process-entry "Adds a dependency graph node's information to map acc." [acc [key val]]
+  (if (empty? val) (acc-set acc :singles key)
+                   (assoc (acc-set acc :dependers key) :dependencies (sets/union (:dependencies acc) val))))
 
-(defn node-by-types [inputs]
-  (let []) (reduce process-entry {} inputs))
+(defn node-by-types "Summarizes a dependency graph into sets by node type." [inputs] (reduce process-entry {} inputs))
 
-(defn -execution-list [tasklist processed unprocessed dependers-map]
+(defn -execution-list "Main task list calculation function." [tasklist processed unprocessed dependers-map]
   (if (not (empty? unprocessed))
     (let [processable     (filter #(empty? (sets/difference (get dependers-map %) processed)) unprocessed)
           new-processed   (sets/union processed processable)
@@ -22,7 +21,7 @@
       (recur tlist new-processed new-unprocessed dependers-map))
     tasklist))
 
-(defn execution-list [dependers-map]
+(defn execution-list "Summarize graph; find start nodes; calculate executable task list itself." [dependers-map]
   (let [categorised (node-by-types dependers-map)
         starts      (sets/union (:singles categorised) (sets/difference (:dependencies categorised) (:dependers categorised)))]
     (-execution-list (into [] starts) starts (:dependers categorised) dependers-map)))
